89095f672cb58636cdd843fba77428ac22b8a9b2
藤原聖 <fujiwarasatoru@fujiwarakiyoshinoMacBook-Air.local>
2021-09-23 10:38:56 +0900

    helloworld(django)

--- backend/django_project/django_app/settings.py
+++ backend/django_project/django_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_app_app.apps.DjangoAppAppConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'django_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':[BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/django_project/django_app_app/urls.py
+++ backend/django_project/django_app_app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
+from .views import demofunc
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('django_app_app.urls'))
+    path('demo/',demofunc,name='demo'),
 ]
